2a8dc2438de68bb9cca6d8992a82e1837318e090
nihaobrat <stekkzgameacc@gmail.com>
2023-06-30 00:02:16 +0300

    Rustlings 1

---
+++ rustlings complete/variables/variables3.rs
@@ -0,0 +1,14 @@
+// variables3.rs
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
+
+
+
+fn main() {
+    let x = 10;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
